c8afc9d204e1d06d146462fa699edf166777d0bf
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2021-01-05 15:32:18 +0100

    Prepare fetch-modules script (no code yet)

--- snippets/opal-fetch-modules.sh
+++ snippets/opal-fetch-modules.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+#
+# This file is part of Opal and has been released into the public domain.
+# SPDX-License-Identifier: CC0-1.0
+# SPDX-FileCopyrightText: 2021 Mirian Margiani
+#
+# See https://github.com/Pretty-SFOS/opal/blob/master/snippets/opal-fetch-modules.md
+# for documentation.
 
 echo "Not yet implemented."
