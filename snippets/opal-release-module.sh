51688ad87bc4cf59cc41a9fa05d35b673352dfbc
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2021-01-05 15:31:32 +0100

    Clean up release script

--- snippets/opal-release-module.sh
+++ snippets/opal-release-module.sh
@@ -19,6 +19,17 @@ TRANSLATE=(Opal)    # which files and directories to translate
 # mkdir translations && lupdate-qt5 Opal -ts "translations/$NAME.ts" && exit 0
 # ------------------------------------------------------------------------------
 
+# ------------------------------------------------------------------------------
+# Edit the copy_files() function if any additional copy steps are necessary.
+# By default, the main Opal directory will be copied with all contents. It might
+# be necessary to exclude certain files that are not meant for distribution.
+function copy_files() {
+    cp -r Opal "$QML_BASE/Opal" || return 1
+}
+# ------------------------------------------------------------------------------
+
+# Prepare version number from git tag, using current date and current commit
+# as fallback values
 if ! git describe --tags 2>/dev/null >/dev/null; then
     VERSION="v$(date +%F)"
     if git rev-parse --short HEAD --verify 2>/dev/null >/dev/null; then
@@ -28,32 +39,33 @@ else
     VERSION="v$(git describe --tags | sed 's/^v//g')"
 fi
 
+# Setup base paths
 PACKAGE="$NAME-$VERSION"
+BUILD_PARENT="build"
+BUILD_ROOT="$BUILD_PARENT/$PACKAGE"
+QML_BASE="$BUILD_ROOT/qml/opal-modules"
+TR_BASE="$BUILD_ROOT/libs/opal-translations"
+# PLUGIN_BASE="$BUILD_ROOT/TODO"
 
+mkdir -p "$BUILD_PARENT" || { echo "error: failed to create base build directory"; exit 1; }
+rm -rf "$BUILD_ROOT" || { echo "error: failed to clear build root"; exit 1; }
+mkdir -p "$BUILD_ROOT" || { echo "error: failed to create build root"; exit 1; }
+mkdir -p "$QML_BASE" "$TR_BASE" || { echo "error: failed to prepare build root"; exit 1; }
+# mkdir -p "$PLUGIN_BASE" || { echo "error: failed to prepare plugin base directory"; exit 1; }
+
+# Update translation catalogs
 lupdate-qt5 "${TRANSLATE[@]}" -ts translations/*.ts || {
-    echo "error: failed to update translations"
-    exit 3
+    echo "error: failed to update translations"; exit 3
 }
 
-mkdir -p build || { echo "error: failed to create build directory"; exit 1; }
-rm -rf "build/$PACKAGE" || { echo "error: failed to clear build root"; exit 1; }
-mkdir -p "build/$PACKAGE"/{opal-translations,opal-modules} || { echo "error: failed to prepare build root"; exit 1; }
-cp translations/*.ts "build/$PACKAGE/opal-translations" || { echo "error: failed to prepare translations"; exit 1; }
-
-# ------------------------------------------------------------------------------
-# Edit the copy_files function if any additional copy steps are necessary.
-# By default, the main Opal directory will be copied with all contents. It might
-# be necessary to exclude certain files that are not meant for distribution.
-function copy_files() {
-    cp -r Opal "build/$PACKAGE/opal-modules/Opal" || return 1
-}
+# Import distribution files
+cp translations/*.ts "$TR_BASE" || { echo "error: failed to prepare translations"; exit 1; }
 copy_files || { echo "error: failed to prepare sources"; exit 1; }
-# ------------------------------------------------------------------------------
 
-cd build
+# Create final package
+cd "$BUILD_PARENT"
 tar -czvf "$PACKAGE.tar.gz" "$PACKAGE" || {
     echo "error: failed to create package"
     exit 2
 }
-
-rm -rf "$PACKAGE"  # build root
+rm -rf "$PACKAGE"  # clear build root
