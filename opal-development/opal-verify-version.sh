0790ba8c37ff7f8c43398b51354a7df8d37078d1
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2021-05-16 14:11:59 +0200

    Improve error reporting

--- opal-development/opal-verify-version.sh
+++ opal-development/opal-verify-version.sh
@@ -11,14 +11,18 @@
 # @@@ FILE VERSION see verify_version
 #
 
+function log() {
+    IFS=' ' printf -- "%s\n" "$*" >&2
+}
+
 function verify_version() {
     # @@@ shared function version: 1.0.0
     local user_version_var="c__FOR_XXXXX_LIB__"
     local opal_version_var="c__OPAL_XXXXX_VERSION__"
 
     if [[ -z "${!user_version_var}" ]]; then
-        echo "error: script compatibility cannot be verified"
-        echo "       make sure $user_version_var is set"
+        log "error: script compatibility cannot be verified"
+        log "       make sure $user_version_var is set"
         exit 1
     fi
 
@@ -36,22 +40,23 @@ function verify_version() {
     local opal_minor="${!opal_version_var#*.}"; opal_minor="${opal_minor%.*}"
     local opal_patch="${!opal_version_var##*.}"
 
-    printf -- "module script: %s, opal library script: %s\n" "${!user_version_var}" "${!opal_version_var}"
-
     if [[ "$opal_major" == 0 && "$major" == "$opal_major" && "$minor" != "$opal_minor" ]]; then
-        echo "warning: unstable API has changed, please check the script"
-        echo "         if everything is fine, update $user_version_var"
+        log "module script: ${!user_version_var}, opal library script: ${!opal_version_var}"
+        log "warning: unstable API has changed, please check the script"
+        log "         if everything is fine, update $user_version_var"
         exit 1
     fi
 
     if (( "$opal_major" > "$major" )); then
-        echo "error: please update the script for the current major library version ($opal_major vs. $major)"
+        log "module script: ${!user_version_var}, opal library script: ${!opal_version_var}"
+        log "error: please update the script for the current major library version ($opal_major vs. $major)"
         exit 1
     fi
 
     if (( "$opal_major" < "$major" || "$opal_minor" < "$minor" )); then
-        echo "warning: the script expects a newer public API ($opal_major.$opal_minor vs. $major.$minor)"
-        echo "         please update the library"
+        log "module script: ${!user_version_var}, opal library script: ${!opal_version_var}"
+        log "warning: the script expects a newer public API ($opal_major.$opal_minor vs. $major.$minor)"
+        log "         please update the library"
         exit 1
     fi
 }
