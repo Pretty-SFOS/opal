955bfdfd7f9b779657e19c48483943b57e65ccb6
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2023-06-14 21:18:07 +0200

    Bump render icons example version to 1.0.0

--- snippets/opal-render-icons-example.sh
+++ snippets/opal-render-icons-example.sh
@@ -2,14 +2,14 @@
 #
 # This file is part of Opal and has been released into the public domain.
 # SPDX-License-Identifier: CC0-1.0
-# SPDX-FileCopyrightText: 2021 Mirian Margiani
+# SPDX-FileCopyrightText: 2021-2023 Mirian Margiani
 #
 # See https://github.com/Pretty-SFOS/opal/blob/main/snippets/opal-render-icons.md
 # for documentation.
 #
-# @@@ keep this line: based on template v0.3.0
+# @@@ keep this line: based on template v1.0.0
 #
-c__FOR_RENDER_LIB__="0.3.0"
+c__FOR_RENDER_LIB__="1.0.0"
 
 # Run this script from the same directory where your icon sources are located,
 # e.g. <app>/icon-src.
